15bf27203dcda88f4621ecdea52746935f0aca5e
Vadim Shpet <vadik.shpet01@gmail.com>
2022-11-28 10:37:37 +0200

    fix mail send

--- mail.php
+++ mail.php
@@ -1,20 +1,26 @@
 <?php
 
-    echo($_POST);
-    $to      = 'nobody@example.com';
-    $subject = 'the subject';
-    $message = 'hello';
-    $headers = 'From: webmaster@example.com'       . "\r\n" .
-                 'Reply-To: webmaster@example.com' . "\r\n" .
-                 'X-Mailer: PHP/' . phpversion();
 
-    mail($to, $subject, $message, $headers);
-    
+$data = explode(',',file_get_contents('php://input'));
+
+$peremennaya = "<table>";
+foreach ($data as $value) {
+    $peremennaya .= '<tr><td>'.$value.'<td></tr>';
+  };
+
+  $peremennaya .="</table>";
+
     $to      = 'vadik.shpet01@gmail.com';
+    $to2      = 'vadik.shpet01@gmail.com';
     $subject = 'the subject';
-    $message = 'hello';
+    $message =$peremennaya;
     $headers = 'From: webmaster@example.com'       . "\r\n" .
                  'Reply-To: webmaster@example.com' . "\r\n" .
                  'X-Mailer: PHP/' . phpversion();
 
+
+
     mail($to, $subject, $message, $headers);
+    mail($to, $subject, $message, $headers);
+
+    header("Location: /thanks.html");
